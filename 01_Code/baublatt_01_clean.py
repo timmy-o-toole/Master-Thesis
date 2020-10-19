fad3a3b21b8905d1a55c2786f5997692ee4ad56b
timmy-o-toole <56508902+timmy-o-toole@users.noreply.github.com>
2020-10-19 10:45:52 +0200

    change

---
+++ 01_Code/baublatt_01_clean.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 09:27:29 2020
+
+@author: Tim
+"""
+import numpy as np
+import pandas as pd
+import os
+import re # for regular expressions
+import seaborn as sns #for graph in correlation heatmap
+import matplotlib.pyplot as plt
+import openpyxl
+
+#%%
